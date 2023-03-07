93a843994b0737ce049a0c7f7e714134c5f353fa
jokerclay <2079209547@qq.com>
2023-03-07 16:46:18 +0800

    进行了组合光和自旋转

--- src/components/ThreeControl.vue
+++ src/components/ThreeControl.vue
@@ -19,7 +19,7 @@ export default {
     // 创建容器
     const container = ref(null);
     // 声明所需的变量
-    let scene, camera, renderer, model, controls, light;
+    let scene, camera, renderer, model, controls;
 
     //  onMounted 的时候
     //      初始化 :
@@ -65,17 +65,45 @@ export default {
       // Create OrbitControls and add them to the camera
       controls = new OrbitControls(camera, renderer.domElement);
       controls.enableDamping = true;
-      controls.dampingFactor = 0.1;
-      controls.rotateSpeed = 1;
-      controls.zoomSpeed = 1.0;
-      controls.panSpeed = 2.0;
+      // controls.dampingFactor = 0.1;
+      // controls.rotateSpeed = 1;
+      // controls.zoomSpeed = 1.0;
+      // controls.panSpeed = 2.0;
 
       // Create a new light and add it to the scene
       // // 创建光源
-      light = new THREE.PointLight(0xffffff, 0.9, 100);
-      light.position.set(2, 2, 5);
+      // light = new THREE.PointLight(0xffffff, 0.9, 100);
+      // light.position.set(2, 2, 5);
       // // 将光源添加到场景
-      scene.add(light);
+      // scene.add(light);
+
+      /*
+      *  用两个 前后方向光  解决 只能看见一面的问题
+      * */
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+      directionalLight.position.set(5, 5, 10);
+      scene.add(directionalLight);
+      //
+      // const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
+      // directionalLight2.position.set(0, 0, -10);
+      // scene.add(directionalLight2);
+
+      /*
+      *  用两个 ambientLight   spotLight 组合 得到较好的结果
+      * */
+
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+      ambientLight .position.set(0, 10, -10);
+      scene.add(ambientLight);
+
+
+      const spotLight = new THREE.SpotLight(0xffffff, 0.5);
+      spotLight.position.set(0, 10, 0);
+      spotLight.target.position.set(4, 4, 0);
+      scene.add(spotLight);
+      scene.add(spotLight.target);
+
+
       window.addEventListener('resize', handleResize);
 
     }
@@ -91,10 +119,15 @@ export default {
             console.log(model);
 
             scene.add(model);
-            model.position.set(0, 0, 0);
+
+            // 设置模型的位置
+            model.position.set(0, 0, -0.3);
+            // 设置模型的大小
+            model.scale.set(1.2, 1.2, 1.2);
 
             // Render the scene
             render();
+            animate();
           },
           (xhr) => {
             console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
@@ -103,6 +136,13 @@ export default {
             console.log("An error happened", error);
           }
       );
+      const animate = () => {
+        requestAnimationFrame(animate);
+        if (model) {
+          model.rotation.y -= 0.01;
+        }
+        renderer.render(scene, camera);
+      };
     };
 
 
